c23eef9494d3e3552a4fe5a808505a038cf40c70
rhoust123 <rhouston@mines.edu>
2024-07-23 16:29:03 -0600

    Added functionality - when user presses screenshot button, program will grab screenshot from clipboard and display it in the window for user viewing

--- ScreenshotApp/ScreenshotApp/ContentView.swift
+++ ScreenshotApp/ScreenshotApp/ContentView.swift
@@ -8,27 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var image: NSImage? = nil
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
             
-            Button("Make a screenshot") {
+            // if image has been set (screenshot taken) show image in window
+            if let image = image {
+                Image(nsImage: image)
+                    .resizable()
+                    .scaledToFit()
+            }
+            
+            // Create a button that prompts the user to take a screenshot
+            Button("Take a screenshot") {
+                
+                // Create a new process called task
                 let task = Process()
+                
+                // Point the task to an already existing macOS command line tool
                 task.executableURL = URL(fileURLWithPath: "/usr/sbin/screencapture")
                 task.arguments = ["-cw"]
                 
+                // Run the task - protect against errors
                 do {
                     try task.run()
                     task.waitUntilExit()
+                    getImageFromPasteboard()
                 } catch {
                     print("could not take screenshot: \(error)")
                 }
             }
         }
         .padding()
+    }
+    
+    // Sets the var image to the image saved to the machine's clipboard
+    func getImageFromPasteboard() {
+     
+        guard NSPasteboard.general.canReadItem(withDataConformingToTypes: NSImage.imageTypes) else { return }
+        
+        guard let image = NSImage(pasteboard: NSPasteboard.general) else { return }
+        
+        self.image = image
     }
 }
 
